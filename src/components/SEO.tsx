64d85ef42f17eb672a3bacfbd4f2cfa7f82231b5
Jassu <jassus@gmail.com>
2025-06-13 14:38:36 -0300

    +CSP

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -193,6 +193,7 @@ export const SEO = ({
       <meta name="googlebot" content="index, follow" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="UV Agency" />
 
