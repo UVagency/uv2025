fb4b6cbe68f568a797624dd2dd7f94953a887b2f
Jassu <45288406+jassus@users.noreply.github.com>
2025-06-27 12:01:04 -0300

    Update OurCompany.tsx

--- src/pages/OurCompany.tsx
+++ src/pages/OurCompany.tsx
@@ -46,7 +46,7 @@ const OurCompany = () => {
     tagline: "UV is the integrated marketing agency that grows your brand through Omnichannel campaigns, crafts Events that move people to create customers, and delivers results through Smart Media.\nOne agency, all in.",
     description: "UV is a Full-service in house independent agency that champions brands through advertising, engineers events that move people to create customers, and drives results through smart media. We specialize in strategy, creativity, content, media and experiences- all connected to help brands grow, inspire, and stay relevant in a constantly shifting world. We're grateful to be working with top brands and passionate teams to create work that resonates - emotionally and measurably. We love what we do and we know that hard work pays off.",
     history: "Founded with a vision to blend cutting-edge technology with creative storytelling, UV has grown from a small team of passionate individuals into a powerhouse of innovation. Our journey has been marked by a relentless pursuit of excellence, a commitment to our clients' success, and a culture that fosters creativity and collaboration. We've embraced challenges, celebrated milestones, and continuously evolved to meet the ever-changing demands of the digital landscape.",
-    videoRecapUrl: "", // Placeholder video
+    videoRecapUrl: "https://vimeo.com/1096947347",
     categories: ["Advertising", "Media", "Events"],
     client: "One Agency, All In. Full Service Independent Agency",
   };
